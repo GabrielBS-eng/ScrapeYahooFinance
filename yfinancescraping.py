ae5a789424b653f951b2707e6156adb037d270e2
Gabriel de Brito Silva <britosilva.eng@gmail.com>
2021-07-17 01:36:36 -0300

    Using yahoo_fin library

--- yfinancescraping.py
+++ yfinancescraping.py
@@ -1,4 +1,20 @@
 import yfinance as yf
+import pandas as pd
+
+from yahoo_fin.stock_info import get_data
 
 bvsp = yf.Ticker("^BVSP")
 hist = bvsp.history(period="5y")
+
+hist.to_excel("output.xlsx")
+
+dfs = pd.read_excel("output.xlsx", sheet_name=None)
+
+
+bvsp2 = get_data('^BVSP', start_date = '16/07/2016', end_date = '16/07/2021', index_as_date = False, interval = "1d")
+
+
+
+#f = open("histBVSP.txt", "w")
+#f.write(hist)
+#f.close()
